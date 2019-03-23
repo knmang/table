229fefe2675e19590e6c473e90bb364b92870e6e
knmang <674436520@qq.com>
2019-03-23 23:48:54 +0800

    一堆垃圾先丢上去

--- js/table2.js
+++ js/table2.js
@@ -37,6 +37,8 @@ class Table extends React.Component {
 		super(props);
 		this.state = {
 			wait: true,
+			scrollHeight: window.innerHeight,
+			CacheData: null,
 		}
 
 		this.handleGetData = this.handleGetData.bind(this);
@@ -46,10 +48,14 @@ class Table extends React.Component {
 
 	componentDidMount() {
 		window.addEventListener('scroll', this.handleGetCache);
+		// this.setState({
+  //       	scrollHeight: window.innerHeight,
+  //       })
 	}
 
   	componentWillUnmount() {
 		window.removeEventListener('scroll', this.handleGetCache);
+		caches.delete('test-cache');
   	}
 
 	handleGetData() {
@@ -62,31 +68,42 @@ class Table extends React.Component {
                 thiz.setState({
                 	wait: false,
                 })
+                localStorage.setItem('data',JSON.stringify(thiz.table.data));
             });
-
-		if('caches' in window) {
-			caches.open('test-cache').then(function(cache) {
-		  		cache.add('js/data2.json');
-			});
-		}
+  
+		// if('caches' in window) {
+		// 	caches.open('test-cache').then(function(cache) {
+		//   		cache.add('js/data2.json');
+		// 	});
+		// }
 
 	}
 
 	handleGetCache() {
-		caches.open('test-cache').then(function(cache) { 
-			cache.keys().then(function(cachedRequests) { 
-		    	console.log(cachedRequests);
+		// caches.open('test-cache').then(function(cache) { 
+		// 	cache.keys().then(function(cachedRequests) { 
+		//     	console.log(cachedRequests);
+		// 	});
+		// });
+		if(818 < scrollY) {
+			this.setState({
+				CacheData: JSON.parse(localStorage.getItem('data')),
 			});
-		});
+		}
 	}
 
 	render() {
+		console.log(this.state.CacheData);
 		return this.state.wait ? e('div', null, null) :
-			e('table', {border: '1', style:{'border-collapse': 'collapse'}, onscroll: this.handleGetCache},
+			e('table', {border: '1', style:{'border-collapse': 'collapse'}},
 				e(Row, {title: this.table.title}, null),
 				this.table.data.map(function(cont, i) {
 					return e(Column, cont, null)
-				}));
+				}),
+					// this.state.CacheData.map(function(cont, i) {
+					// 	return e(Column, cont, null)
+					// })
+				);
 	}
 }
 
