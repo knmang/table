b7ae29c9af1d4729a3562ef4b2ac5e2e88d04468
jcry <674436520@qq.com>
2019-08-29 23:05:57 +0800

    修改，这几天有些私事忙了一下

--- js/table.js
+++ js/table.js
@@ -23,19 +23,24 @@ class Column extends React.Component {
 	}
 
 	handleGetIdTop() {
-		if(this.props.list[0] == this.props.id) {
-			this.props.setTop(this.refs.id.offsetTop);
-		}
+		// if(this.props.list[0] == this.props.id) {
+		// 	this.props.setTop(this.refs.id.offsetTop);
+		// }
 	}
 
 	render() {
 		var cont = [];
 
-        for (var a in this.props.list) {
-        	if(null != this.props.list[a])
-            	cont.push(e('td', {ref: 'id'}, this.props.list[a]));
-        	if(0 == a)
-        		this.handleGetIdTop();
+        // for (var a in this.props.list) {
+        // 	if(null != this.props.list[a])
+        //     	cont.push(e('td', {ref: 'id'}, this.props.list[a]));
+        // 	if(0 == a)
+        // 		this.handleGetIdTop();
+        // }
+
+        for (var a in this.props) {
+        	if(null != this.props[a])
+        	cont.push(e('th', null, this.props[a]));
         }
 
         return e('tr', null, cont);
@@ -66,78 +71,86 @@ class Table extends React.Component {
 		this.handleGetData();
 	}
 
-	componentDidMount(){
-		window.addEventListener('scroll', this.handleScroll);
+	componentDidMount() {
 		this.props.onRef(this);
 	}
 
-  	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
-  	}
-
+	//判断滚动条状况
   	handleScroll() {
-  		// console.log(this.scrollTop);
-		// var {clientHeight} = this.refs.table;
-		// var isBottom = this.refs.table.clientHeight + this.refs.table.scrollTop + 50;
-		var isBottom = window.innerHeight = window.scrollY;
-		// console.log(isBottom);
+		var {clientHeight,scrollTop,scrollHeight} = this.refs.table;
+		var isBottom = clientHeight + scrollTop === scrollHeight;
+			
+		if(isBottom){
+			this.handleGetNext();
+		}
 
-		// if(isBottom >= this.refs.table.scrollHeight){
-		// 	this.handleGetNext();
-		// }
-
-		if(0 == isBottom) {
-			this.handleGetBefore();		
+		if(0 == this.refs.table.scrollTop) {
+			this.handleGetBefore();	
 		}
 	}
 
 	handleSetTop (offsetTop){
-		this.refs.table.scrollTop = offsetTop;
+		// this.refs.table.scrollTop = offsetTop;
 	}
 
+	//下翻
 	handleGetNext() {
 		if(this.state.max > this.state.count){
+			//缓存数据读取完，要重新拿数据
 			this.setState((prevState) => ({
 				skip: prevState.skip + this.state.count,
 				min: 0,
 				max: this.props.max,
 				type: 1,
-			}));
-			this.handleGetData();
+			}), () => {
+				this.handleGetData();
+			})		
 		}else{
+			//缓存有数据，下翻
 			this.setState((prevState) => ({
-	        	min: prevState.min + Math.floor(this.state.add*0.8),
-	        	max: prevState.max + this.state.add,
-	        	type: 3,
-	        }));
-	        this.handleShowData();
+	        	// min: prevState.min + Math.floor(this.state.add*0.8),
+	        	min: prevState.max - 1,
+	        	max: prevState.max + this.state.add - 1,
+	        	// type: 3,
+	        }), () => {
+				this.handleShowData();
+	        })
 		}	
 	}
 
+	//上翻
 	handleGetBefore() {
 		if(0 >= this.state.min){
+			//缓存数据读取完，要重新拿数据
 			if(this.state.skip > this.props.skip){
 				this.setState((prevState) => ({
 					skip: prevState.skip - this.state.count,
 					min: this.props.max,
 					max: this.props.max * 2,
 					type: 2,
-				}))
-				this.handleGetData();
+				}), () => {
+					this.handleGetData();
+				})			
 			}			
 		}else{
+			//缓存有数据，上翻，1、剩下数据够再次递减，2、不够递减。
 			this.state.max > this.props.max ? this.setState((prevState) => ({
+				// min: prevState.min - Math.floor(this.state.add*0.8),
+				min: prevState.min + this.state.add - 2,
+				max: prevState.max - this.state.add - 2,
+				// type: 4,
+			}),() => {
+				this.handleShowData();
+			}) : this.setState((prevState) => ({
 				min: prevState.min - Math.floor(this.state.add*0.8),
-				max: prevState.max - this.state.add,
-				type: 4,
-			})) : this.setState((prevState) => ({
-				min: prevState.min - Math.floor(this.state.add*0.8),
-				type: 4,
-			}))
-			this.handleShowData();
+				// type: 4,
+			}), () => {
+				this.handleShowData();
+			})		
 		}
 	}
 
+	//滚动后获取新数据
 	handleShowData() {
 		var getbuffer =  JSON.parse(sessionStorage.getItem('buffer')).data;
 		var change = [];
@@ -147,33 +160,49 @@ class Table extends React.Component {
 			if (getbuffer[i])
 			change.push(getbuffer[i]);
 		}
-		if(1 == this.state.type){
-			id = this.showData[this.showData.length - 1][0];
+		// if(1 == this.state.type){
+		// 	//不够缓存数据型下翻
+		// 	id = this.showData[this.showData.length - 1][0];
+		// 	this.showData = (this.showData.slice(-10,-1)).concat(this.showData.slice(-1)).concat(change);
+		// }else if(2 == this.state.type){
+		// 	//不够缓存数据型上翻
+		// 	id = this.showData[0][0];
+		// 	this.showData = change.concat(this.showData.slice(0,9));
+		// }else if(3 == this.state.type){
+		// 	//下翻
+		// 	id = this.showData[this.showData.length - 1][0];
+		// 	this.showData = change;
+		// }else if(4 == this.state.type){
+		// 	//上翻
+		// 	id = this.showData[0][0];
+		// 	this.showData = change;
+		// }else{
+		// 	//首次
+		// 	this.showData = change;
+		// }
+
+		if(1 == this.state.type) {
+			//不够缓存数据型下翻
 			this.showData = (this.showData.slice(-10,-1)).concat(this.showData.slice(-1)).concat(change);
-		}else if(2 == this.state.type){
-			id = this.showData[0][0];
+		}else if(2 == this.state.type) {
+			//不够缓存数据型上翻
 			this.showData = change.concat(this.showData.slice(0,9));
-		}else if(3 == this.state.type){
-			id = this.showData[this.showData.length - 1][0];
-			this.showData = change;
-		}else if(4 == this.state.type){
-			id = this.showData[0][0];
-			this.showData = change;
-		}else{
+		}else {
+			//正常翻滚
 			this.showData = change;
 		}
-
+		// console.log(this.showData);
 		this.setState({
 			type: 0,
-			id: id,
+			// id: id,
 		})
 	}
 
-	handleButton(btnType) {
-		console.log(window);
-		// btnType ? this.refs.table.scrollTop = 0 : this.refs.table.scrollTop = this.refs.table.scrollHeight;
+	handleButton(btnType) {	
+		btnType ? this.refs.table.scrollTop = 0 : this.refs.table.scrollTop = this.refs.table.scrollHeight;
 	}
 
+	//获取缓存数据
 	handleGetData() {
 		var thiz = this;
         var skip = this.state.skip;
@@ -193,15 +222,17 @@ class Table extends React.Component {
 
 	render() {
 		var thiz = this;
-		var id = this.state.id;
+		// var id = this.state.id;
 
 		return this.state.wait ? e('div', null, null) :
-			e('table', {style: {'margin-top' : '55px'}},
-				e(Row, {title: this.table.title}, null),
-				this.showData.map(function(list, i) {
-					return e(Column, {list: list, setTop: thiz.handleSetTop, id: id}, null)
-				}));
+			e('div', {ref: 'table', style: {overflowY: 'scroll', height:'100vh', white:'100vw'}, onScroll: this.handleScroll},
+				e('table', {style: {'margin-top' : '55px',}},
+					e(Row, {title: this.table.title}, null),
+					this.showData.map(function(list, i) {
+						// return e(Column, {list: list, setTop: thiz.handleSetTop, id: id}, null)
+						return e(Column, list, null)
+					})));
 	}
 }
 
-// ReactDOM.render(e(Main, null, null), document.getElementById('root'));
+// ReactDOM.render(e(Table, {onRef: this.onRef, link: 'https://home.chnlab.com/table', skip: 100, add: 5, max: 15}, null)), document.getElementById('root'));
